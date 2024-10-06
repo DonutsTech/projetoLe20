c3fd71ff9e792f8c2f9db1cddf5536068a5d978e
Neto Russo <netoacrn1@gmail.com>
2024-10-06 03:23:58 -0300

    Subindo o backgroud em video

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react';
+import Style from './Background.module.scss';
+
+const Background = () => {
+
+  return (
+    <>
+      <div className={Style.overlay} />
+      <video autoPlay muted loop className={Style.video}>
+        <source src="/assets/video/bg.mp4" type="video/mp4" />
+      </video>
+    </>
+  )
+};
+
+export default Background;
