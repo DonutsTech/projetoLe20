bbe4c29f014846065c57daf1b28a658112515277
Neto Russo <netoacrn1@gmail.com>
2024-10-06 03:23:12 -0300

    subindo o diretório central do site

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Chamada from "@/components/Chamada";
+import Mapa from "@/components/Mapa";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Chamada />
+      <Mapa />
+    </div>
+  );
+}
