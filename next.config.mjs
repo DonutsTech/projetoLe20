a5904be997eead3c9e65470fe40341eaf2fc9df6
Neto Russo <netoacrn1@gmail.com>
2024-10-06 03:19:04 -0300

    Arquivo de configuração

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import path from 'path';
+import withVideos from 'next-videos';
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), './src/styles')],
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  webpack(config) {
+    return config;
+  },
+};
+
+export default withVideos(nextConfig, {
+  assetPrefix: '/assets/video',
+});
