1dc809537c7f29b94a18b81c06d5583042ed610d
Neto Russo <netoacrn1@gmail.com>
2024-10-07 23:53:32 -0300

    Adicionando o formulario

---
+++ src/components/Contato/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import Formulario from '../Formulario';
+import Style from './Contato.module.scss';
+
+
+const Contato = () => { 
+
+  return (
+    <div className={Style.contato}>
+      <Formulario />
+    </div>
+  )
+};
+
+export default Contato;
