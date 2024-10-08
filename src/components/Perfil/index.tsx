777f1b8c006c3e02acf457b91668921b566b3e5a
Neto Russo <netoacrn1@gmail.com>
2024-10-07 23:52:43 -0300

    Adicionando o texto

--- src/components/Perfil/index.tsx
+++ src/components/Perfil/index.tsx
@@ -10,7 +10,7 @@ const Perfil = () => {
       <Blobs />
       <div className={Style.perfilTextos}>
         <h3>Lúcia Helena Bucchianeri</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis expedita at illo nihil, rerum nam, pariatur laboriosam quisquam perspiciatis sequi quas perferendis debitis veniam doloribus, ullam sunt! Harum, placeat incidunt.</p>
+        <p>Há 36 anos no mercado de vendas, amo ser representante comercial. Cada dia traz novos desafios. Prefiro o contato humano das vendas face a face, que nenhuma tecnologia substitui.</p>
       </div>
     </div>
   )
