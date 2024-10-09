32d83aa333e13b34632235491783428d95da92da
Neto Russo <netoacrn1@gmail.com>
2024-10-09 00:42:49 -0300

    adicionando a pagina de contatos

--- src/app/contatos/page.tsx
+++ src/app/contatos/page.tsx
@@ -1,12 +1,12 @@
 import Contato from "@/components/Contato";
 import Perfil from "@/components/Perfil";
-
+import Style from "./Contatos.module.scss";
 
 
 export default function Contatos() {
 
   return (
-    <div>
+    <div className={Style.page}>
       <Perfil />
       <Contato />
     </div>
