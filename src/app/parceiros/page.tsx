801b7deb18eefc4d5e6193e4cf0e9c2aa93484db
Neto Russo <netoacrn1@gmail.com>
2024-10-09 00:44:08 -0300

    adicionando a pagina de parceiros

---
+++ src/app/parceiros/page.tsx
@@ -0,0 +1,12 @@
+import Style from './Parceiros.module.scss';
+import GridCard from '@/components/GridCard';
+
+
+export default function Parceiros() { 
+
+  return (
+    <div className={Style.page}>
+      <GridCard />
+    </div>
+  )
+};
