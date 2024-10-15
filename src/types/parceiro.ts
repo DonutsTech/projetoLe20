531d7f351a002a6c231985e9db122624b8cdecbc
Neto Russo <netoacrn1@gmail.com>
2024-10-15 01:04:12 -0300

    ajustes

--- src/types/parceiro.ts
+++ src/types/parceiro.ts
@@ -1,11 +1,16 @@
 // import { StaticImageData } from "next/image";
 
+export interface ICatalogo {
+  nome: string,
+  catalogo: string,
+}
+
 export interface IParceiro {
   'nome': string,
   'imagens': Array<string> | [],
   'descricao': string,
   'linkSite'?: string,
   'linkCompartilhar'?: string,
-  'catalogo': Array<string> | [],
+  'catalogo': Array<ICatalogo> | [],
   'tips': Array<string> | [],
 }
