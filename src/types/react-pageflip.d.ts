5b11261ca3d195706fbf06c4eb5995edaa4bff29
Neto Russo <netoacrn1@gmail.com>
2024-10-16 21:04:29 -0300

    Adicionando tipagem do componente externo

---
+++ src/types/react-pageflip.d.ts
@@ -0,0 +1,23 @@
+declare module 'react-pageflip' {
+  import { Component } from 'react';
+
+  interface HTMLFlipBookProps {
+    width: number;
+    height: number;
+    size?: string;
+    minWidth?: number;
+    maxWidth?: number;
+    minHeight?: number;
+    maxHeight?: number;
+    showCover?: boolean;
+    flippingTime?: number;
+    startPage?: number;
+    drawShadow?: boolean;
+    usePortrait?: boolean;
+    className?: string;
+    style?: React.CSSProperties;
+    children?: React.ReactNode;
+  }
+
+  export default class HTMLFlipBook extends Component<HTMLFlipBookProps> {}
+}
