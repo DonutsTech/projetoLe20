2a518563963a16739c0b33946cf4a61d3dc3ab20
Neto Russo <netoacrn1@gmail.com>
2024-10-16 23:08:32 -0300

    ajustes 1610 2308

--- src/components/LeitorPdf/index.tsx
+++ src/components/LeitorPdf/index.tsx
@@ -13,35 +13,45 @@ interface ILeitorPdfProps {
 const LeitorPdf = ({ file }: ILeitorPdfProps) => {
   const [pages, setPages] = useState<string[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  const [isSinglePage, setIsSinglePage] = useState(false); // Estado para definir se é uma página ou dupla
+
+  // Função para verificar a largura da tela
+  const handleResize = () => {
+    if (window.innerWidth < 780) {
+      setIsSinglePage(true);
+    } else {
+      setIsSinglePage(false);
+    }
+  };
+
+  useEffect(() => {
+    // Verificar o tamanho da tela ao carregar
+    handleResize();
+
+    // Adicionar um listener para alterações no tamanho da tela
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const fetchPdf = async () => {
-      setLoading(true);
-      setError(null); // Reseta o erro antes de tentar buscar novamente
-
       try {
+        setLoading(true);
         const response = await fetch('/api/download', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ url: file }),
         });
-
-        if (!response.ok) {
-          throw new Error('Erro ao fazer o download do arquivo PDF.');
-        }
-
         const data = await response.json();
-
         if (data.fileContent) {
           const pdfImages = await convertPdfToImages(data.fileContent);
           setPages(pdfImages);
-        } else {
-          throw new Error('O conteúdo do arquivo PDF não foi encontrado.');
         }
       } catch (error) {
         console.error('Erro ao carregar o PDF:', error);
-        setError('Ocorreu um erro ao carregar o catálogo.');
       } finally {
         setLoading(false);
       }
@@ -54,23 +64,19 @@ const LeitorPdf = ({ file }: ILeitorPdfProps) => {
     return <div className={styles.loading}>Carregando catálogo...</div>;
   }
 
-  if (error) {
-    return <div className={styles.error}>{error}</div>;
-  }
-
   return (
     <div className={styles.container}>
       <HTMLFlipBook 
-        width={550} 
-        height={733} 
-        size="stretch" 
-        minWidth={300} 
-        maxWidth={1000} 
-        minHeight={420} 
+        width={isSinglePage ? 400 : 550} // Ajusta a largura para páginas únicas ou duplas
+        height={isSinglePage ? 533 : 733} // Ajusta a altura proporcional
+        size="stretch"
+        minWidth={300}
+        maxWidth={1000}
+        minHeight={420}
         maxHeight={1533}
         showCover={true}
         drawShadow={true}
-        usePortrait={false}
+        usePortrait={isSinglePage} // Exibe em modo retrato quando for uma página
         flippingTime={500}
         startPage={0}
         className={styles.book}
