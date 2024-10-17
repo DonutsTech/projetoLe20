8058bd893c2edbbc2d0652058c820c8709a1cdf8
Neto Russo <netoacrn1@gmail.com>
2024-10-16 21:01:25 -0300

    Ajustando a api

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -1,35 +1,44 @@
-import { NextRequest, NextResponse } from 'next/server';
-import fetch from 'node-fetch';
+import { NextResponse } from 'next/server';
+import axios from 'axios';
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get('id');
-
-  if (!id) {
-    return NextResponse.json({ error: 'ID do PDF não fornecido.' }, { status: 400 });
-  }
-
+export async function POST(request: Request) {
   try {
-    const googleDriveUrl = `https://drive.google.com/uc?export=download&id=${id}`;
-    // const drive = `https://mega.nz/file/EQoUnYbD#MYtKUwamrxUIBrLQ-${id}`;
+    const { url } = await request.json();
 
-    const response = await fetch(googleDriveUrl);
-    // const response = await fetch(drive);
-
-    if (!response.ok) {
-      throw new Error('Erro ao baixar o PDF');
+    if (!url) {
+      return NextResponse.json({ error: 'URL não fornecida' }, { status: 400 });
     }
 
-    const pdfBuffer = await response.arrayBuffer();
+    // Extrai o ID do arquivo do URL do Google Drive
+    const fileId = extractFileId(url);
 
-    return new NextResponse(pdfBuffer, {
-      status: 200,
+    if (!fileId) {
+      return NextResponse.json({ error: 'URL do Google Drive inválida' }, { status: 400 });
+    }
+
+    // URL para download direto do Google Drive
+    const directDownloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+    // Faz o download do arquivo
+    const response = await axios.get(directDownloadUrl, {
+      responseType: 'arraybuffer',
       headers: {
         'Content-Type': 'application/pdf',
       },
     });
+
+    // Converte o arquivo para Base64
+    const base64 = Buffer.from(response.data, 'binary').toString('base64');
+
+    return NextResponse.json({ fileContent: base64 });
   } catch (error) {
-    console.error('Erro ao baixar o PDF:', error);
-    return NextResponse.json({ error: 'Erro ao baixar o PDF' }, { status: 500 });
+    console.error('Erro ao baixar o arquivo:', error);
+    return NextResponse.json({ error: 'Erro ao baixar o arquivo' }, { status: 500 });
   }
+}
+
+function extractFileId(url: string): string | null {
+  const regex = /\/d\/([a-zA-Z0-9_-]+)/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
 }
