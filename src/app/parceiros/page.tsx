7c51328ef8b38770bb804d7c34397aca1b05715c
Neto Russo <netoacrn1@gmail.com>
2024-10-17 00:31:01 -0300

    Tentando deploy 1710 0030

--- src/app/parceiros/page.tsx
+++ src/app/parceiros/page.tsx
@@ -1,12 +1,22 @@
+if (typeof window !== 'undefined' && !Promise.withResolvers) {
+  Promise.withResolvers = function <T>() {
+    let resolve!: (value: T | PromiseLike<T>) => void;
+    let reject!: (reason?: unknown) => void; // Mudança de any para unknown
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
+
 import Style from './Parceiros.module.scss';
 import GridCard from '@/components/GridCard';
 
-
 export default function Parceiros() { 
-
   return (
     <div className={Style.page}>
       <GridCard />
     </div>
-  )
+  );
 };
