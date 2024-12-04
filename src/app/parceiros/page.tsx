7c69b22c19e21ca1ebeafce1a2458666d9c1c086
Milena Leme <milenaleme4@hotmail.com>
2024-12-03 22:03:25 -0300

    atualizando o editor

--- src/app/parceiros/page.tsx
+++ src/app/parceiros/page.tsx
@@ -2,7 +2,7 @@
 if (typeof window !== 'undefined' && !Promise.withResolvers) {
   Promise.withResolvers = function <T>() {
     let resolve!: (value: T | PromiseLike<T>) => void;
-    let reject!: (reason?: unknown) => void; 
+    let reject!: (reason?: unknown) => void;
     const promise = new Promise<T>((res, rej) => {
       resolve = res;
       reject = rej;
@@ -13,33 +13,25 @@ if (typeof window !== 'undefined' && !Promise.withResolvers) {
 
 import Style from './Parceiros.module.scss';
 import GridCard from '@/components/GridCard';
+import Tipos from '@/components/Tipos';
 import listaParceiros from "@/data/listaParceiros.json";
 import { useState } from 'react';
 // import { IParceiro } from '@/types/parceiro';
 // import { useState } from 'react';
 
-const tipos: string[] = ['Papelarias', 'Brinquedos', 'Pedagogicos', 'Papelaria Fofa', 'Festas', 'Presentes', 'Puericultura', 'Licenças']
-
-export default function Parceiros() { 
+export default function Parceiros() {
   const [tipoSelecionado, setSelecionado] = useState<string | null>(null)
 
-  const selecionouTipo = (nome: string) => {
+  const selecionouTipo = (nome: string | null) => {
     setSelecionado(nome === tipoSelecionado ? null : nome)
   }
 
   const lista  = listaParceiros;
 
-
   return (
     <div className={Style.page}>
-      {
-        tipos.map((nome, index) => (
-          <button key={index} type='button' onClick={() => selecionouTipo(nome)}>
-            {nome}
-          </button>
-        ))
-      }
-      <GridCard 
+      <Tipos tipoSelecionado={tipoSelecionado} selecionouTipo={selecionouTipo} />
+      <GridCard
       parceiros={tipoSelecionado === null ? lista : lista.filter(({tips}) => tips.includes(tipoSelecionado))}
       />
     </div>
